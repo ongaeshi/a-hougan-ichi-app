8230fbd3e183e89f58ecf7db0fb691077fb0a368
ongaeshi <ongaeshi0621@gmail.com>
2026-06-20 17:33:06 +0900

    Fix unused import error

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect, useRef } from 'react';
+import { useState, useEffect } from 'react';
 import { speak } from './utils/speech';
 import { playCorrectSound, playIncorrectSound } from './utils/audio';
 
